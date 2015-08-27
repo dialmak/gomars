c4447391c39d13eb1316602f2256a2d444f29e0c
dialmak <ostapvoip@gmail.com>
2015-08-28 01:21:28 +0300

    add files
    add

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$('document').ready(function() {
+	var options = { videoId: '_9JxSrwlTA4', start: 30 , playButtonClass: 'tubular-play', pauseButtonClass: 'tubular-pause', width: $(window).width(300)};
+	$('#wrapper').tubular(options);
+
+	/*ratio: 16/9 ;// usually either 4/3 or 16/9 -- tweak as needed
+	videoId: 'ZCAnLxRvNNc'; // toy robot in space is a good default, no?
+	mute: true;
+	repeat: true;
+	width: $(window).width(); // no need to override
+	wrapperZIndex: 99;
+	playButtonClass: 'tubular-play';
+	pauseButtonClass: 'tubular-pause';
+	muteButtonClass: 'tubular-mute';
+	volumeUpClass: 'tubular-volume-up';
+	volumeDownClass: 'tubular-volume-down';
+	increaseVolumeBy: 10 ;// increment value; volume range is 1-100
+	start: 0;// starting position in seconds*/
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
